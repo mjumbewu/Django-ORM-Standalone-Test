f6ed7dd7845c862f8adce8592ec6fddf139a5ed6
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-12-15 19:05:45 -0800

    More notes

--- standalone/testapp/models.py
+++ standalone/testapp/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Construct Django models like normal. When you're done, you can use syncdb, or
 # a migration management app (which I recommend) like South to manage your
 # database.
+#
+# With South, if you change your models, run:
+#
+#     python manage.py schemamigration <appname> --auto
+#     python manage.py migrate <appname>
+#
 class TestModel (models.Model):
     myfield = models.TextField()
     myinteger = models.IntegerField(null=True)
